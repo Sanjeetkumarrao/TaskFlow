3ac982d88ee10bc0fe48112e8e3f478bead48a33
Sanjeetkumarrao <sanjeetkumarbharti2018@gmail.com>
2026-04-02 02:40:40 +0530

    MongoDB connection setup kiya

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,21 @@
-import express from 'express';
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import connectDB from "./config/db.js";
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Server is running !')
+connectDB();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend is running...");
 });
 
-app.listen(5000, () => {
-    console.log(`Server is running on PORT : http://localhost:5000 `);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on port ${PORT}`);
 });
