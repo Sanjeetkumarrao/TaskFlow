822e9d6cb0e97ea4d0692b993f571444c124c763
Sanjeetkumarrao <sanjeetkumarbharti2018@gmail.com>
2026-04-01 11:30:29 +0530

    Nodemon setup kiya

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,9 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Server is running !!')
+    res.send('Server is running !')
 });
 
 app.listen(5000, () => {
-    console.log(`Server is running on PORT : http//localhost:5000 `);
+    console.log(`Server is running on PORT : http://localhost:5000 `);
 });
