724044696d9336887464b6a779e68301e138b871
Sanjeetkumarrao <sanjeetkumarbharti2018@gmail.com>
2026-04-03 02:11:11 +0530

    getTasks API complete

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -19,3 +19,21 @@ export const createTask = async (req , res ) => {
         res.status(500).json({message:  error.message})
     }
 }
+
+
+export const getTasks = async (req ,res ) => {
+    try {
+        const userId = req.userId;
+        const task = await Task.find({user: userId});
+    
+        return res.status(200).json({
+            message: "Tasks fetched .",
+            task
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: "Server error ",
+            error: error.message
+        })
+    }
+}
