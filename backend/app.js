a18a2741dbac25ab893e4cf0d2a4df3b97e855b7
Sanjeetkumarrao <sanjeetkumarbharti2018@gmail.com>
2026-04-03 00:26:37 +0530

    Auth middleware complete

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,5 @@
 import express from 'express';
-import authRoutes from "./routes/authRoutes.js"
-
+import authRoutes from "./routes/authRoutes.js";
 const app = express();
 
 app.use(express.json());
@@ -8,6 +7,7 @@ app.use(express.json());
 app.get('/', (req , res ) => {
     res.send("Server is running")
 });
+
 
 app.use("/api/auth", authRoutes);
 
