efe7f2c6bb52e1c12286b0d5c9c1eefc9dff18b5
Sanjeetkumarrao <sanjeetkumarbharti2018@gmail.com>
2026-04-02 15:39:19 +0530

    Login API complete with JWT

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+import jwt from 'jsonwebtoken';
 
 //Register Controller
 export const registerUser = async(req , res ) => {
@@ -34,3 +35,45 @@ export const registerUser = async(req , res ) => {
         res.status(500).json({message: error.message})
     }
 };
+
+
+// LOGIN controller
+
+export const loginUser = async (req , res) => {
+    try {
+        const {email, password} = req.body;
+    
+        const existingEmail = await User.findOne({email});
+        if(!existingEmail){
+            return res.status(404).json({
+                message: "User not found with this Email"
+            })
+        }
+    
+        const isMatch = await bcrypt.compare(password, existingEmail.password);
+    
+        if(!isMatch){
+            return res.status(401).json({
+                message: "Incorrect password !!"
+            })
+        }
+    
+        const token = jwt.sign(
+            { userId: existingEmail._id },
+            process.env.JWT_SECRET,
+            { expiresIn: "7d"}
+        );
+    
+        return res.status(200).json({
+            message: "Login Successful",
+            user : {
+                id: existingEmail._id,
+                name: existingEmail.name,
+                email: existingEmail.email,
+            },
+            token: token
+        })
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+}
