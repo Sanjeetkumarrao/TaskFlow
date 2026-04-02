7fe2fd8adbda8e05ea01d2b18d957c112c40455b
Sanjeetkumarrao <sanjeetkumarbharti2018@gmail.com>
2026-04-03 02:35:33 +0530

    updateTask API complete

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -32,8 +32,30 @@ export const getTasks = async (req ,res ) => {
         })
     } catch (error) {
         res.status(500).json({
-            message: "Server error ",
-            error: error.message
+            message: error.message
+        })
+    }
+}
+
+
+export const updateTask = async (req , res ) => {
+    try {
+        const taskId = req.params.id;
+        const data = req.body;
+    
+        const updatedTask = await Task.findByIdAndUpdate(
+            taskId,
+            data,
+            {new: true}
+        )
+    
+        return res.status(200).json({
+            message: "Task Updated Successful",
+            updatedTask
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
         })
     }
 }
--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createTask, getTasks } from '../controllers/taskController.js';
+import { createTask, getTasks, updateTask } from '../controllers/taskController.js';
 
 const router = express.Router();
 
 router.post("/", createTask);
 router.get("/", getTasks)
+router.put("/:id", updateTask)
 
 export default router;
