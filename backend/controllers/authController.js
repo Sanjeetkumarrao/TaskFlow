f4add462045780902728d9955f31c4be0fceebc8
Sanjeetkumarrao <sanjeetkumarbharti2018@gmail.com>
2026-04-12 19:18:24 +0530

    updated to check name email and password coming in req.body should not be empty

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -8,6 +8,10 @@ import transporter from "../config/emailConfig.js";
 export const registerUser = async(req , res ) => {
     try {
         const {name, email, password} = req.body;
+
+        if (!name || !email || !password) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
     
         const existingUser = await User.findOne({email});
         if(existingUser){
